efff4f429cbf3c685921bf6b5bbef0a57faf9456
Codemermoz <kouakoumermoz033@gmail.com>
2023-02-07 07:56:04 -0800

    0x15-files_io/Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,6 @@
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -18,14 +17,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(fielname, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
+
 	if (text_content)
 	{
 		for (nletters = 0; text_content[nletters]; nletters++)
 			;
+
 		rwr = write(fd, text_content, nletters);
 
 		if (rwr == -1)
